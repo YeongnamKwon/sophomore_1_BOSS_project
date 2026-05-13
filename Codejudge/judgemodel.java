a4384fb7b97f07294da0d24ece01d5bca3b76f89
justcoding0206 <rnjsdudska123@gmail.com>
2026-05-13 17:44:02 +0900

    정답, 오답 판별

--- Codejudge/judgemodel.java
+++ Codejudge/judgemodel.java
@@ -15,12 +15,36 @@ public class judgemodel {
 
         	Users_answercode user = new Users_answercode();
 
-            Files.write( Paths.get("Users_answercode.java"),user.User_code.getBytes());
-            String content = Files.readString(Paths.get("Users_answercode.java"));
+            Files.write( Paths.get("Users_answercode.java"), user.User_code.getBytes());
             
-            System.out.println(content);
+            compiler com = new compiler();
+            boolean success = com.compile();
 
-        } catch (Exception e) {
+            if(success) {	
+            	System.out.println("컴파일 성공");
+            	runner run = new runner();
+
+                String output =  run.run();
+                
+                judge j = new judge();
+
+                boolean result = j.check(output, "3");
+
+                if(result) {
+                	System.out.println("정답");
+                } 
+                else {
+                	System.out.println("오답 ");
+                	System.out.println("시스템 정답 : " + "3");
+                	System.out.println("실행 결과 : " + output);
+                }
+            }
+            else {
+            	System.out.println("컴파일 실패");
+            	return;
+            }
+        } 
+        catch (Exception e) {
         	log.severe(e.getMessage());
         	e.printStackTrace();
         }
