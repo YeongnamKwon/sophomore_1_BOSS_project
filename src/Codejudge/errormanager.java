65c9bee94caf287b67fb88ac4119bab5be54ab37
영남 권 <gyn5557281@naver.com>
2026-05-13 20:26:02 +0900

    runtime에러 추가

---
+++ src/Codejudge/errormanager.java
@@ -0,0 +1,14 @@
+package Codejudge;
+
+public class errormanager {
+
+    public boolean success;
+
+    public boolean runtimeError;
+
+    public boolean timeout;
+
+    public String output;
+
+    public String errorMessage;
+}
