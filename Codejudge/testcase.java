b175b78cae47a45dca0fb765b0fff856831ec8e7
justcoding0206 <rnjsdudska123@gmail.com>
2026-05-13 18:48:30 +0900

    testcase입력출력 관리

---
+++ Codejudge/testcase.java
@@ -0,0 +1,12 @@
+package Codejudge;
+
+public class testcase {
+
+    String input;
+    String answer;
+
+    public testcase(String input, String answer) {
+    	this.input = input;
+        this.answer = answer;
+    }
+}
