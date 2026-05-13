aa4b89e8c98a8313592f4b4ad1b1bf3d5ac41b95
justcoding0206 <rnjsdudska123@gmail.com>
2026-05-13 18:50:43 +0900

    testcase 입력 받기 수정

--- Codejudge/runner.java
+++ Codejudge/runner.java
@@ -7,7 +7,7 @@ import java.io.OutputStreamWriter;
 
 public class runner {
 
-    public String run() {
+    public String run(String input) {
 
         try {
         	ProcessBuilder pb =new ProcessBuilder("java","Users_answercode.java");
@@ -15,21 +15,32 @@ public class runner {
             Process process = pb.start();
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             BufferedWriter writer =new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
-            writer.write("1 2");
+            BufferedReader errorReader = new BufferedReader( new InputStreamReader(process.getErrorStream()));
+            
+            writer.write(input);
             writer.newLine();
 
             writer.flush();
             writer.close();
             
             StringBuilder output = new StringBuilder();
+            StringBuilder error = new StringBuilder();
 
             String line;
 
             while((line = reader.readLine()) != null) {
             	output.append(line);
             }
+            
+            while((line = errorReader.readLine()) != null) {
+            	error.append(line);
+            }
 
             process.waitFor();
+            
+            if(error.length() > 0) {
+            	return "Runtime Error : " + error;
+            }
             
             return output.toString();
 
