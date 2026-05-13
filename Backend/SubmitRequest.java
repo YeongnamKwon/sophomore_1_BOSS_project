5d421b5ef23bdb7ba5affdc0e8dff60a144eef9d
Jeon <tkfka1014@naver.com>
2026-05-13 17:22:54 +0900

    fix: 채점 오타 수정 및 SubmitResponse 파일 삭제

--- Backend/SubmitRequest.java
+++ Backend/SubmitRequest.java
@@ -1,31 +1,31 @@
 package Backend;
 
 public class SubmitRequest {
-    private String sourceCode;
-    private String input;
-    private String expectedOutput;
+    private String sourceCode; // 제출한 소스코드
+    private String input; // 입력값
+    private String expectedOutput; // 기대 출력값
 
-    public String getSourceCode(){
+    public String getSourceCode(){ // s.C값 반환
         return sourceCode;
     }
 
-    public void setSourceCode(String sourceCode){
+    public void setSourceCode(String sourceCode){ // s.C값 저장
         this.sourceCode = sourceCode;
     }
 
-    public String getInput(){
+    public String getInput(){ // input 값을 반환
         return input;
     }
 
-    public void setInput(String input){
+    public void setInput(String input){ // input 값을 저장
         this.input = input;
     }
 
-    public String getExpectedOutput(){
+    public String getExpectedOutput(){ // e.O 값을 반환
         return expectedOutput;
     }
 
-    public void setExpectedOutput(String expectedOutput){
+    public void setExpectedOutput(String expectedOutput){ // e.O 값을 저장
         this.expectedOutput = expectedOutput; 
     }
 }
